347ea386ebccd0798fd49edf8fabedfca2882566
Admin <ac@Admins-MacBook-Pro.local>
2019-02-08 11:10:34 -0600

    Updated variables

--- www/TestIonicPlugin.js
+++ www/TestIonicPlugin.js
@@ -2,10 +2,10 @@ var exec = require('cordova/exec');
 
 var PLUGIN_NAME = "TestIonicPlugin"; // This is just for code completion uses.
 
-var YourPluginName = function() {}; // This just makes it easier for us to export all of the functions at once.
+var TestIonicPlugin = function() {}; // This just makes it easier for us to export all of the functions at once.
 // All of your plugin functions go below this. 
 // Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
-YourPluginName.pluginFunction = function(onSuccess, onError) {
+TestIonicPlugin.pluginFunction = function(onSuccess, onError) {
    exec(onSuccess, onError, PLUGIN_NAME, "pluginFunction", []);
 };
 module.exports = TestIonicPlugin;
